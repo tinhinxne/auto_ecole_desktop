0a748e4d5ee8268b56c71cf2bfd757654d318020
Tinhinane Belarbi <tinhinane.belarbi@se.univ-bejaia.dz>
2026-03-30 19:59:35 +0100

    Sauvegarde des modifications avant de passer à main

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -27,9 +27,10 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import AgendaPage from "./pages/Agenda";
-
+import Payments from "./pages/Payments";
 import Layout from "./layout/Layout";
 import Examens from "./pages/Examens";
+import Condidats from "./pages/condidats";
 
 const App = () => {
   return (
@@ -37,11 +38,11 @@ const App = () => {
       <Routes>
 
         {/* LOGIN (sans layout) */}
-        <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<Examens />} />
 
         {/* ROUTES AVEC SIDEBAR */}
         <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<AgendaPage/>} />
           <Route path="/agenda" element={<AgendaPage />} />
         </Route>
 
