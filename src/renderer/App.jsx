cebc58a0108b2f588b4da52a783f0f4853705e77
Tinhinane Belarbi <tinhinane.belarbi@se.univ-bejaia.dz>
2026-03-31 21:39:21 +0100

    Fix CSP

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -25,33 +25,32 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import SignIn from "./pages/SignIn";
+import Access from "./pages/Access";
 import Dashboard from "./pages/Dashboard";
 import AgendaPage from "./pages/Agenda";
 import Payments from "./pages/Payments";
 import Layout from "./layout/Layout";
 import Examens from "./pages/Examens";
 import Condidats from "./pages/condidats";
+import Moniteur from "./pages/Moniteur";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
+   <Router>
+  <Routes>
+    <Route path="/" element={<Examens />} />
 
-        {/* LOGIN (sans layout) */}
-        <Route path="/" element={<Examens />} />
-
-        {/* ROUTES AVEC SIDEBAR */}
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/agenda" element={<AgendaPage />} />
-        </Route>
-
-        {/* <Route path="/" element={<SignIn />} /> */}
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        {/* <Route path="/examens" element={<Examens />} /> */}
-         {/* <Route path="/" element={<Examens />} /> */}
-      </Routes>
-    </Router>
+    <Route element={<Layout />}>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/candidats" element={<Condidats />} />
+      <Route path="/moniteur" element={<Moniteur />} />
+      <Route path="/agenda" element={<AgendaPage />} />
+      <Route path="/examens" element={<Examens />} />
+      <Route path="/payments" element={<Payments />} />
+      <Route path="/settings" element={<Settings />} />
+    </Route>
+  </Routes>
+</Router>
   );
 };
 
