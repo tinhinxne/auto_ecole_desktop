879c93fc12ff78bd060712d3c3107b3b0ef8d22f
hadjer-b <BerkaniHadjer1880@gmail.com>
2026-04-01 19:49:07 +0100

    fix fontawseome dependency, use npm install

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// if it doesn't work
+// do this to download fontawesome: npm install @fortawesome/fontawesome-free @fontsource/outfit
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fontsource/outfit/300.css";
+import "@fontsource/outfit/400.css";
+import "@fontsource/outfit/500.css";
+import "@fontsource/outfit/600.css";
+import "@fontsource/outfit/700.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
