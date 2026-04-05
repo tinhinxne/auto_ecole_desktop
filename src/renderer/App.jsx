c895a3d9a902875fd7be27c8c008fa4f761dde50
Sonia Benazouz <sosoweb001@gmail.com>
2026-04-05 01:36:23 +0100

    parametres

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import SignIn from "./pages/SignIn";
+import Access from "./pages/Access";
 import Dashboard from "./pages/Dashboard";
 import Condidats from "./pages/condidats";
 import Moniteur from "./pages/Moniteur";
@@ -10,24 +11,32 @@ import AgendaPage from "./pages/Agenda";
 import Payments from "./pages/Payments";
 import Examens from "./pages/Examens";
 import Layout from "./layout/Layout";
-import  Parametres from "./pages/parametres";
+import Parametres from "./layout/parametres";
 
 const App = () => {
   return (
     <HashRouter>
-     <Routes>
-  <Route path="/" element={<Navigate to="/parametres" replace />} />
-  
-  <Route element={<Layout />}>
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/candidats" element={<Condidats />} />
-    <Route path="/moniteur" element={<Moniteur />} />
-    <Route path="/agenda" element={<AgendaPage />} />
-    <Route path="/examens" element={<Examens />} />
-    <Route path="/payments" element={<Payments />} />
-    <Route path="/parametres" element={<Parametres />} />
-  </Route>
-</Routes>
+      <Routes>
+
+        {/* ── Redirection racine → connexion ── */}
+        <Route path="/" element={<Navigate to="/connexion" replace />} />
+
+        {/* ── Page connexion — sans sidebar ── */}
+        <Route path="/connexion" element={<SignIn />} />
+        <Route path="/access" element={<Access />} />
+
+        {/* ── Pages protégées — avec sidebar via Layout ── */}
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/candidats" element={<Condidats />} />
+          <Route path="/moniteur" element={<Moniteur />} />
+          <Route path="/agenda" element={<AgendaPage />} />
+          <Route path="/examens" element={<Examens />} />
+          <Route path="/payments" element={<Payments />} />
+           <Route path="/parametres" element={<Parametres />} />
+        </Route>
+
+      </Routes>
     </HashRouter>
   );
 };
