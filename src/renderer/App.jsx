d56e1a4f7523c06defd1a2191a64e8210ce21e71
Amina <amina.albane@se.univ-bejaia.dz>
2026-04-06 21:32:23 +0100

    g

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,39 +1,13 @@
-// App.jsx
 import React from "react";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-import SignIn from "./pages/SignIn";
-import Dashboard from "./pages/Dashboard";
-import Condidats from "./pages/condidats";
-import Moniteur from "./pages/Moniteur";
-import AgendaPage from "./pages/Agenda";
-import Payments from "./pages/Payments";
-import Examens from "./pages/Examens";
-import Layout from "./layout/Layout";
-import MesCandidats from ""
+import AgendaMoniteur from "./pages/AgendaMoniteur";
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
-
-        {/* ── Redirection racine → connexion ── */}
-        <Route path="/" element={<Navigate to="/MesCondidats" replace />} />
-
-        {/* ── Page connexion — sans sidebar ── */}
-        <Route path="/connexion" element={<SignIn />} />
-
-        {/* ── Pages protégées — avec sidebar via Layout ── */}
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/candidats" element={<Condidats />} />
-          <Route path="/moniteur" element={<Moniteur />} />
-          <Route path="/agenda" element={<AgendaPage />} />
-          <Route path="/examens" element={<Examens />} />
-          <Route path="/payments" element={<Payments />} />
-          
-        </Route>
-
+        <Route path="*" element={<AgendaMoniteur />} />
       </Routes>
     </HashRouter>
   );
