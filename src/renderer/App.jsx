b70c08b16190a2ee85b22fce30e4c6688f7bc9e4
Amina <amina.albane@se.univ-bejaia.dz>
2026-04-06 23:17:28 +0100

    goot

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate} from "react-router-dom";
 
 import SignIn from "./pages/SignIn";
 import Access from "./pages/Access";
